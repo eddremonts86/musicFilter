0b3f3111acd2bf950d937bde78d0a25a6f707398
Edd Remonts <eddremonts86@gmail.com>
2020-07-24 13:47:28 +0200

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <div class="home_hero">
+      <v-spacer></v-spacer>
+      <div class="home_content">
+        <h1>Make your own Playlist</h1>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </p>
+        <v-btn large>Generate now</v-btn>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: () => ({})
+};
+</script>
---
+++ src/components/navBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar app class="nav_bg">
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+    <v-spacer></v-spacer>
+    <ul class="nav_list">
+      <li>Home</li>
+      <li>Services</li>
+      <li>About us</li>
+      <li>Contact</li>
+    </ul>
+  </v-app-bar>
+</template>
